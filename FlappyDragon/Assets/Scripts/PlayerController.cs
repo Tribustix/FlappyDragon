3ab36bd8d0639d06242103559f474769903652df
tribustix <tribustix@gmail.com>
2018-03-24 21:29:40 +0100

    Added Player Functionality

---
+++ FlappyDragon/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody2D playerRigidBody2D;
+	public float upForce = 200f;
+	private float rotationSpeedLimit = 10f;
+	private float maxAngleToRotate = 30f;
+	private float angle;
+	
+
+	private void Start () {
+		playerRigidBody2D = GetComponent<Rigidbody2D>();
+	}
+	
+	private void Update () {
+		
+		float calculateTime = playerRigidBody2D.velocity.y / rotationSpeedLimit;
+
+		if(Input.GetMouseButtonDown(0)){
+			playerRigidBody2D.velocity = Vector2.zero;
+			playerRigidBody2D.AddForce(new Vector2(0, upForce));
+		}
+
+		if(playerRigidBody2D.velocity.y >=0f){
+			angle = Mathf.Lerp(0, maxAngleToRotate, calculateTime);
+		}else{
+			angle = Mathf.Lerp(0, -maxAngleToRotate, -calculateTime);
+		}
+		
+		playerRigidBody2D.MoveRotation(angle);
+	}
+
+	private void Jump(){
+		
+	}
+
+}
