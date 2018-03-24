e01102813d9760b860b6cf54c5547744736fac17
tribustix <tribustix@gmail.com>
2018-03-24 22:36:06 +0100

    Added Obstacles

---
+++ FlappyDragon/Assets/Scripts/Obstacles.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacles : MonoBehaviour {
+
+
+	private float obstaclesVelocity = -3f;
+	private float obstaclesDistance = 11f;
+
+
+	
+	void Update () {
+		
+		transform.position += new Vector3(obstaclesVelocity * Time.deltaTime, 0f, 0f);
+
+		if(transform.position.x < -7f){
+			transform.position = setObstacle();
+		}
+	}
+
+	private Vector3 setObstacle(){
+		Vector3 temporalPosition = transform.position + (new Vector3(obstaclesDistance,0f,0f));
+		temporalPosition.y = Random.Range(4.17f, 8f);
+		return temporalPosition;
+	}
+}
