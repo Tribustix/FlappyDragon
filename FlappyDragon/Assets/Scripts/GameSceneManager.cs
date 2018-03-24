e39d8cadd36a7f3a62573a0640355d70e5b463f8
tribustix <tribustix@gmail.com>
2018-03-24 20:12:27 +0100

    Added a GameSceneManager
    I've added a Game Scene Manager to go from one Scene to another and I've added a new Scene called Level. This Scene will contain the gameplay.
    Finally I've changed the file .gitignore.

---
+++ FlappyDragon/Assets/Scripts/GameSceneManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSceneManager : MonoBehaviour {
+
+	public static GameSceneManager Instance;
+
+	private void Awake() {
+		Instance = this;
+	}
+	public void LoadScene(string sceneToLoad){
+		SceneManager.LoadScene(sceneToLoad);
+	}
+
+	public void QuitGame(){
+		Application.Quit();
+		Debug.Log("Se ha salido del juego");
+	}
+}
